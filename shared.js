0cbabd6891488caa092950780fee21b86acd9025
Arun Singh <arunsinghsnd@gmail.com>
2021-06-28 07:53:09 +0530

    added the eventlistener

--- shared.js
+++ shared.js
@@ -1,5 +1,16 @@
 var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+// console.dir(selectPlanButtons);
 
 // console.dir(backdrop);
 
-backdrop.style.display = "block";
+//backdrop.style.display = "block";
